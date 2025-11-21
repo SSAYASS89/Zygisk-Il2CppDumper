ae68edf389f95cc1a60226a70e94b0997ffb6b93
SSAYASS89 <109432522+SSAYASS89@users.noreply.github.com>
2025-11-21 15:50:53 +0100

    Change GamePackageName from Pokemon to Subway Surfer
    com.kiloo.subwaysurf

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "jp.pokemon.pokemontcgp"
+#define GamePackageName "com.kiloo.subwaysurf"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
